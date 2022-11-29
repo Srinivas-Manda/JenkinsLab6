827fe77641e93e6ef1af8c711009678f814ef183
srinivas <srinivas911032@gmail.com>
2022-11-29 18:34:25 +0530

    Correction to master 1

--- Test.py
+++ Test.py
@@ -3,17 +3,17 @@
 import unittest
 
 from Subtraction import Diff
-from Task2 import Quot
+from Division import Quot
 
 class Maths(unittest.TestCase):
 
     def test_Subtraction(self):
         diff = Diff(20, 10)
-        self.assertEqual(diff, 30)
+        self.assertEqual(diff, 10)
 
     def test_Division(self):
         div = Quot(20, 10)
-        self.assertEqual(div, 200)
+        self.assertEqual(div, 2)
 
 if __name__ == '__main__':
     unittest.main()
