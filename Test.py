dcb680d53edaa933a8b8287c2840f4b099b15e3b
srinivas <srinivas911032@gmail.com>
2022-11-29 18:35:44 +0530

    Correction made

--- Test.py
+++ Test.py
@@ -9,11 +9,11 @@ class Maths(unittest.TestCase):
 
     def test_Subtraction(self):
         diff = Diff(10, 10)
-        self.assertEqual(diff, 10)
+        self.assertEqual(diff, 0)
 
     def test_Division(self):
         div = Quot(10, 10)
-        self.assertEqual(div, 2)
+        self.assertEqual(div, 1)
 
 if __name__ == '__main__':
     unittest.main()
