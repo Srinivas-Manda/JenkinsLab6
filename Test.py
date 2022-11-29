638e73b1fac8ed0e650b69d7122f29d9cb611307
srinivas <srinivas911032@gmail.com>
2022-11-29 18:36:24 +0530

    Another correct case

--- Test.py
+++ Test.py
@@ -8,12 +8,12 @@ from Division import Quot
 class Maths(unittest.TestCase):
 
     def test_Subtraction(self):
-        diff = Diff(10, 10)
-        self.assertEqual(diff, 0)
+        diff = Diff(50, 10)
+        self.assertEqual(diff, 40)
 
     def test_Division(self):
-        div = Quot(10, 10)
-        self.assertEqual(div, 1)
+        div = Quot(30, 10)
+        self.assertEqual(div, 3)
 
 if __name__ == '__main__':
     unittest.main()
